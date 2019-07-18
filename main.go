c36f6c15e81cc0420104beef0154535eed3a54e4
Manav Sethi <dev.manav.sethi@gmail.com>
2019-07-18 19:46:43 +0530

    added ssl

--- main.go
+++ main.go
@@ -57,7 +57,7 @@ func StartServer() error {
 
 	log.Println("Listening at : " + os.Getenv("PORT"))
 
-	err := http.ListenAndServe(":" + os.Getenv("PORT"),  Multiplexer)
+	err := http.ListenAndServeTLS(":" + os.Getenv("PORT"), "ssl/certificate.crt","ssl/private.key", Multiplexer)
 	if err != nil {
 		return err
 	}
